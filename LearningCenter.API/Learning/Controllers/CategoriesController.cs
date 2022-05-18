f5c8af19fa2a822bfcf3265a7724b2fad9bd0de5
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-17 20:59:15 -0500

    feat(categories): Implemented Service Layer.

---
+++ LearningCenter.API/Learning/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+using AutoMapper;
+using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Learning.Domain.Services;
+using LearningCenter.API.Learning.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningCenter.API.Learning.Controllers;
+
+[Route("/api/v1/[controller]")] 
+public class CategoriesController : ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+    private readonly IMapper _mapper;
+    
+
+    public CategoriesController(ICategoryService categoryService, IMapper mapper)
+    {
+        _categoryService = categoryService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<CategoryResource>> GetAllSync()
+    {
+        var categories = await _categoryService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryResource>>(categories);
+        
+        return resources;
+    }
+}
