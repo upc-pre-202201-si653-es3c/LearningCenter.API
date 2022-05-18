e5f3d49842f251313b907637d03da29bd47c87b3
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-17 20:05:32 -0500

    feat(categories): Added DTO for Incoming and Outgoing resources.

--- LearningCenter.API/Shared/Domain/Services/Communication/BaseResponse.cs
+++ LearningCenter.API/Shared/Domain/Services/Communication/BaseResponse.cs
@@ -1,6 +1,22 @@
 namespace LearningCenter.API.Shared.Domain.Services.Communication;
 
-public class BaseResponse
+public abstract class BaseResponse<T>
 {
-    
+    protected BaseResponse(T resource)
+    {
+        Success = true;
+        Resource = resource;
+        Message = string.Empty;
+    }
+
+    protected BaseResponse(string message)
+    {
+        Success = false;
+        Resource = default;
+        Message = message;
+    }
+
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public T Resource { get; set; }
 }
