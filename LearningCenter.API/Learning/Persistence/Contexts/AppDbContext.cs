8a560876f6237e7b9e48dda5adeecf7da4763838
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-24 20:04:31 -0500

    feat(tutorials): Implemented Table Mapping Rules.

--- LearningCenter.API/Learning/Persistence/Contexts/AppDbContext.cs
+++ LearningCenter.API/Learning/Persistence/Contexts/AppDbContext.cs
@@ -11,15 +11,34 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Tutorial> Tutorials { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
+        // Categories
+        
         builder.Entity<Category>().ToTable("Categories");
         builder.Entity<Category>().HasKey(p => p.Id);
         builder.Entity<Category>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
         builder.Entity<Category>().Property(p => p.Name).IsRequired().HasMaxLength(30);
+        
+        // Relationships
+        
+        builder.Entity<Category>()
+            .HasMany(p => p.Tutorials)
+            .WithOne(p => p.Category)
+            .HasForeignKey(p => p.CategoryId);
+        
+        // Tutorials
+
+        builder.Entity<Tutorial>().ToTable("Tutorials");
+        builder.Entity<Tutorial>().HasKey(p => p.Id);
+        builder.Entity<Tutorial>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Tutorial>().Property(p => p.Title).IsRequired().HasMaxLength(50);
+        builder.Entity<Tutorial>().Property(p => p.Description).HasMaxLength(120);
+        
         
         // Apply Snake Case Naming Conventions
         
