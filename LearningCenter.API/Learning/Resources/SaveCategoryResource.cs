166449304ecb9b7e202fd70fe236c18520a4a6c7
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-27 20:24:55 -0500

    feat(categories): Added Incoming Object Validation rules.

--- LearningCenter.API/Learning/Resources/SaveCategoryResource.cs
+++ LearningCenter.API/Learning/Resources/SaveCategoryResource.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LearningCenter.API.Learning.Resources;
 
 public class SaveCategoryResource
 {
+    [Required]
+    [MaxLength(30)]
     public string Name { get; set; }
 }
