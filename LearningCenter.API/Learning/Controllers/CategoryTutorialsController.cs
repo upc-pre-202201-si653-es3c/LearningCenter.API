7b950f837ff9d79058f6d62143c0607c9df3f0a7
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-31 20:07:04 -0500

    feat(tutorials): Added Inbound Service for nested resources.

---
+++ LearningCenter.API/Learning/Controllers/CategoryTutorialsController.cs
@@ -0,0 +1,31 @@
+using AutoMapper;
+using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Learning.Domain.Services;
+using LearningCenter.API.Learning.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningCenter.API.Learning.Controllers;
+
+[ApiController]
+[Route("/api/v1/categories/{categoryId}/tutorials")] 
+public class CategoryTutorialsController : ControllerBase
+{
+    private readonly ITutorialService _tutorialService;
+    private readonly IMapper _mapper;
+
+    public CategoryTutorialsController(ITutorialService tutorialService, IMapper mapper)
+    {
+        _tutorialService = tutorialService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<TutorialResource>> GetAllByCategoryId(int categoryId)
+    {
+        var tutorials = await _tutorialService.ListByCategoryIdAsync(categoryId);
+
+        var resources = _mapper.Map<IEnumerable<Tutorial>, IEnumerable<TutorialResource>>(tutorials);
+
+        return resources;
+    }
+}
