a5f950d63bbc93af5a51a0a414ff366d3a1d257c
Angel Velasquez <pcisavel@upc.edu.pe>
2022-05-31 20:49:02 -0500

    fix: Replace port value and deleted status property temporally.

--- LearningCenter.API/Learning/Domain/Models/Tutorial.cs
+++ LearningCenter.API/Learning/Domain/Models/Tutorial.cs
@@ -5,7 +5,7 @@ public class Tutorial
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public bool Status { get; set; }
+    
     
     // Relationships
     
