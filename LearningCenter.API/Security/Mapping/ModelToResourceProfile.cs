cfd65c4a979d216a9e17653411534e69f814f9ce
Angel Velasquez <pcisavel@upc.edu.pe>
2022-06-10 20:31:08 -0500

    feat(security): Added Object Mapping rules.

---
+++ LearningCenter.API/Security/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using LearningCenter.API.Security.Domain.Models;
+using LearningCenter.API.Security.Domain.Services.Communication;
+using LearningCenter.API.Security.Resources;
+
+namespace LearningCenter.API.Security.Mapping;
+
+public class ModelToResourceProfile : Profile
+{
+    public ModelToResourceProfile()
+    {
+        CreateMap<User, AuthenticateResponse>();
+
+        CreateMap<User, UserResource>();
+    }
+}
