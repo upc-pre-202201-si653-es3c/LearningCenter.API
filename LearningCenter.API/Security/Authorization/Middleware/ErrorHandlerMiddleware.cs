b46f2bf61a305bef21d8162d0edaabc1ec263706
Angel Velasquez <pcisavel@upc.edu.pe>
2022-06-14 20:13:20 -0500

    feat(security): Added Error Handler Middleware.

---
+++ LearningCenter.API/Security/Authorization/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,50 @@
+using System.Net;
+using System.Net.Mime;
+using System.Text.Json;
+using LearningCenter.API.Security.Exceptions;
+
+namespace LearningCenter.API.Security.Authorization.Middleware;
+
+public class ErrorHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception error)
+        {
+            var response = context.Response;
+            response.ContentType = MediaTypeNames.Application.Json;
+
+            switch (error)
+            {
+                case AppException e:
+                    // Custom Application Exception
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    break;
+                
+                case KeyNotFoundException e:
+                    // Not found error
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
+                
+                default:
+                    // Unhandled error
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            var result = JsonSerializer.Serialize(new { message = error?.Message });
+            await response.WriteAsync(result);
+        }
+    }
+}
